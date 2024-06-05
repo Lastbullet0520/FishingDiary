ddd199d8ac604418ccbb154059ca830e352e670b
Lastbullet0520 <wjswndgk@gmail.com>
2024-06-05 18:12:22 +0900

    savepoint, searchableDropdown 구현; thanks to Breens-Mbaka, but it was old.

--- app/src/main/java/com/lastbullet/fishingdiary/MainScreenActivity.kt
+++ app/src/main/java/com/lastbullet/fishingdiary/MainScreenActivity.kt
@@ -5,20 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,18 +25,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.lastbullet.fishingdiary.ui.theme.FishingDiaryTheme
-import kotlin.math.round
 
 
 class MainScreenActivity : ComponentActivity() {
@@ -70,10 +62,13 @@ fun Greeting2() {
                 .weight(0.9f)
                 .background(color = Color(0xffd3f0f0))
         ) {
-            IconButton(onClick = { /*TODO*/ }, modifier = Modifier.align(BiasAlignment(0.9f,0.95f))
-                .clip(CircleShape)
-                .size(80.dp),
-                colors = IconButtonDefaults.iconButtonColors(Color.Red)) {
+            IconButton(
+                onClick = { /*TODO*/ }, modifier = Modifier
+                    .align(BiasAlignment(0.9f, 0.95f))
+                    .clip(CircleShape)
+                    .size(80.dp),
+                colors = IconButtonDefaults.iconButtonColors(Color.Red)
+            ) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_add_a_photo_24),
                     contentDescription = null,
